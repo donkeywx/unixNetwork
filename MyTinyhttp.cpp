694f020ecbcc72496fc4a8dc7045d2476135457f
donkeywx <wangk9294@163.com>
2019-01-24 22:31:02 +0800

    create tinyhttp file

---
+++ MyTinyhttp.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: MyTinyhttp.cpp
+	> Author:donkeywx 
+	> Mail:wangkang 
+	> Created Time: Thu 24 Jan 2019 10:30:28 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
